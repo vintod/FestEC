1c8fa88c8347164e06ed0c7bf8c4eb57d1a860b2
test <1>
2018-04-13 16:25:11 +0800

    网络框架接口创建

--- latte-core/src/main/java/com/diabin/latte/net/RestCreator.java
+++ latte-core/src/main/java/com/diabin/latte/net/RestCreator.java
@@ -18,6 +18,7 @@ public class RestCreator {
         private static final String BASE_URL = (String) Latte.getConfigurations().get(ConfigType.API_HOST.name());
         private static final Retrofit RETROFIT_CLIENT = new Retrofit.Builder()
                 .baseUrl(BASE_URL)
+                .client(OKHttpHolder.OK_HTTP_CLIENT)
                 .addConverterFactory(ScalarsConverterFactory.create())
                 .build();
     }
